c4a1f1116a31748be956419ec42cec287e30c143
Marcus Vinicius Kuquert <marcus.kuquert@icloud.com>
2017-06-14 07:41:48 -0300

    Add rgb UIColor extension

--- DeepX/Extensions.swift
+++ DeepX/Extensions.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2017 Marcus Vinicius Kuquert. All rights reserved.
 //
 
+import UIKit
 import Foundation
-
 
 extension Formatter {
     static let withSeparator: NumberFormatter = {
@@ -21,5 +21,23 @@ extension Formatter {
 extension Integer {
     var formattedWithSeparator: String {
         return Formatter.withSeparator.string(for: self) ?? ""
+    }
+}
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
     }
 }
